2fd4a0f8ca4d0c7351104dda0c584b2103dde046
tigerweili <tigerweili@tencent.com>
2022-11-26 13:53:20 +0800

    update some doc

--- store/src/main/java/org/apache/rocketmq/store/pop/PopCheckPoint.java
+++ store/src/main/java/org/apache/rocketmq/store/pop/PopCheckPoint.java
@@ -21,24 +21,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PopCheckPoint {
+    // 本次pop拉取消息的起始 message queue offset
     @JSONField(name = "so")
     private long startOffset;
+    // pop发生的时间，单位毫秒
     @JSONField(name = "pt")
     private long popTime;
+    // 这批消息的不可见时间
     @JSONField(name = "it")
     private long invisibleTime;
+    // 当前这批消息的 ack情况， 通过二进制运算，记录了每个消息是否被ack了
     @JSONField(name = "bm")
     private int bitMap;
+    // 本次pop拉取到的消息条数
     @JSONField(name = "n")
     private byte num;
+    // 本次pop拉取的queue id
     @JSONField(name = "q")
     private byte queueId;
+    // topic
     @JSONField(name = "t")
     private String topic;
+    // 消费者组id
     @JSONField(name = "c")
     private String cid;
+    // 恢复 位点？？？
     @JSONField(name = "ro")
     private long reviveOffset;
+    // 消息位点差 xxx - yyy ???
+    //
     @JSONField(name = "d")
     private List<Integer> queueOffsetDiff;
     @JSONField(name = "bn")
@@ -178,7 +189,7 @@ public class PopCheckPoint {
     @Override
     public String toString() {
         return "PopCheckPoint [topic=" + topic + ", cid=" + cid + ", queueId=" + queueId + ", startOffset=" + startOffset + ", bitMap=" + bitMap + ", num=" + num + ", reviveTime=" + getReviveTime()
-            + ", reviveOffset=" + reviveOffset + ", diff=" + queueOffsetDiff + ", brokerName=" + brokerName + "]";
+                + ", reviveOffset=" + reviveOffset + ", diff=" + queueOffsetDiff + ", brokerName=" + brokerName + "]";
     }
 
 }
